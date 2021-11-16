34f4acf619763f809fcb250ff353f858d5a814c6
yuvrav <yuv@whatify.ai>
2021-11-16 22:49:39 +0200

    port`

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 app.get('/', (req, res) => {
    res.send('Hello World!')
 })
